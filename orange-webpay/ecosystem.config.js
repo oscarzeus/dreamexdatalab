c5b4e75b27b6edcbf92b99adebcea144ad30a46e
Oussein Ibrahim SYLLA <imoudreamentreprises@gmail.com>
2025-10-19 19:00:04 +0000

    Create ecosystem.config.js

---
+++ orange-webpay/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'orange-webpay',
+    script: 'server.js',
+    cwd: '/var/www/orange-webpay',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 8080
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 8080
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+};
